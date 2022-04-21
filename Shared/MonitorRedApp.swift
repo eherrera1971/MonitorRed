ccd7372fe38585a5513883d47630aa0d424d64b8
Eduardo Herrera <Eduardo@7seeds.cl>
2022-04-21 16:07:17 -0400

    Initial Commit

---
+++ Shared/MonitorRedApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonitorRedApp.swift
+//  Shared
+//
+//  Created by Eduardo Herrera Barros on 21-04-22.
+//
+
+import SwiftUI
+
+@main
+struct MonitorRedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
